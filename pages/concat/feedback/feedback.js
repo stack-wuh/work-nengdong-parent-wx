6d1653f60de6082215d0f719a1fb0b03fedd81a5
wuhong <wuh131420@foxmail.com>
2018-07-30 10:47:39 +0800

    2017-07-30-add
    意见反馈

--- pages/concat/feedback/feedback.js
+++ pages/concat/feedback/feedback.js
@@ -1,4 +1,5 @@
 // pages/concat/feedback/feedback.js
+const app = getApp()
 Page({
 
   /**
@@ -15,52 +16,21 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  submit(e){
+    let data = e.detail.value
+    if(!data.content){
+      app.toastMsg(-1,'请编辑内容')
+      return
+    }
+    app.apiPost('opinion/addOption.do',data).then(res=>{
+      app.toastMsg(res.status,res.msg)
+      if(res.status == 0){
+        setTimeout(() => {
+          wx.switchTab({
+            url:'/pages/index/index'
+          })
+        }, 1000);
+      }
+    })
   }
 })
