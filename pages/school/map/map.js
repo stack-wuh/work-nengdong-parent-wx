5543a7387e7d0b39d71e626689618d32562bb61c
wuhong <wuh131420@foxmail.com>
2018-08-01 18:58:05 +0800

    2018-08-01-add

--- pages/school/map/map.js
+++ pages/school/map/map.js
@@ -1,12 +1,16 @@
 // pages/school/map/map.js
+const app = getApp()
+const WxParse = require('../../../utils/wxParse/wxParse.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    latitude:12,
-    longitude:14,
+    latitude:30.612941,
+    longitude:114.362908,
+    scale:16,
+    data:[]
   },
 
   /**
@@ -15,7 +19,19 @@ Page({
   onLoad: function (options) {
     let self = this
     this.mapCtx = wx.createMapContext('myMap')
+    this.fetchData()
   },
 
-  
+  fetchData(){
+    app.apiPost('recruit/getSchoolRoute.do').then(res=>{
+        res.data.map(item => {
+          var article = item.content
+          WxParse.wxParse('article','html',article,this,20)
+        })
+        this.setData({
+          data:res.data
+        })
+    })
+  }
+
 })
